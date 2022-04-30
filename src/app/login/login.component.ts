83194513d07c5712fe84b7e8f25b73dfd308b820
Jose David <josedavid.m.jara@gmail.com>
2022-04-30 16:32:57 +0300

    Added validation for login form.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,30 +9,56 @@ import { TokenStorageService } from '../_services/token-storage.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  form: any = {
-    username: null,
-    password: null
-  };
+
+  form!: FormGroup;
+  submitted = false;
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   username: any;
   roles: string[] = [];
 
-
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
       this.isLoggedIn = true;
       this.roles = this.tokenStorage.getUser().roles;
       this.username = this.tokenStorage.getUser().username;
-    } 
+    }
+    this.form = this.formBuilder.group(
+      {
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(3),
+            Validators.maxLength(20)
+          ]
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40)
+          ]
+        ]
+      }
+    );
+  }
+
+  get formControls(): { [key: string]: AbstractControl } {
+    return this.form.controls;
   }
 
   onSubmit(): void {
-    const { username, password } = this.form;
-    this.authService.login(username, password).subscribe({
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.authService.login(this.form.value['username'], this.form.value['password']).subscribe({
       next: (data) => {
         this.tokenStorage.saveToken(data.accessToken);
         this.tokenStorage.saveRefreshToken(data.refreshToken);
@@ -45,6 +72,11 @@ export class LoginComponent implements OnInit {
         this.isLoginFailed = true;
       }
     });
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.form.reset();
   }
 
   reloadPage(): void {
